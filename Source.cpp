0cb4dac059aa31302a78fc6038ed43d0ad22f3f3
ashc0des <50849834+ashc0des@users.noreply.github.com>
2024-10-09 16:53:21 -0400

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-// Program that mimics a calculator
+// Continuation of last project
 
 #include <iostream>
 using namespace std;
@@ -6,41 +6,34 @@ using namespace std;
 int main()
 {
 	//setting up variables
-	char oper;
 	int num1, num2, total;
+	char oper;
 	total = 0;
 
 	//getting input
-	cout << "Please input the first number: ";
+	cout << "Please enter the first number: ";
 	cin >> num1;
-	cout << "Please input the second number: ";
+	cout << "Please enter the second number: ";
 	cin >> num2;
-	cout << "Please enter the operator (+, -, *, or /): ";
+	cout << "Please enter the operator (+, -, *, /): ";
 	cin >> oper;
 
-	//case switch
-	switch (oper)
-	{
-		case '+':
-			total = num1 + num2;
-			break;
-		case '-':
-			total = num1 - num2;
-			break;
-		case '*':
-			total = num1 * num2;
-			break;
-		case '/':
-			total = num1 / num2;
-			break;
-		default:
-			cout << "The given operator doesn't exist." << endl;
-	}
+	//conditional
+	if (oper == '+')
+		total = num1 + num2;
+	else if (oper == '-')
+		total = num1 - num2;
+	else if (oper == '*')
+		total = num1 * num2;
+	else if (oper == '/')
+		total = num1 / num2;
+	else
+		cout << "The given operator doesn't exist." << endl;
 
 	//display results
 	cout << "First number: " << num1 << endl;
 	cout << "Second number: " << num2 << endl;
-	cout << "Operation: " << oper << endl;
+	cout << "Operator: " << oper << endl;
 	cout << "Result: " << total;
 
 	return 0;
